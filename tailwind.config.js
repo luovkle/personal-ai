c52da4d9668cfb94a39f98f9dc04c868f830fb4f
luovkle <roberto.silva.dev@protonmail.com>
2023-08-14 14:47:25 -0600

    💄 Update UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./app/templates/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bodybg: "#25294a",
+        cardbg: "#202442",
+        chatbg: "#2D325A",
+        iconfill: "#919bc3",
+        hovericonfill: "#E9EBF9",
+        textcolor: "#919bc3",
+        hovertextcolor: "#E9EBF9"
+      },
+    },
   },
   plugins: [],
 };
