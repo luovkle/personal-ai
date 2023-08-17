d59d159c185b1f3ea69ade7c60df622d8fbe2af1
luovkle <roberto.silva.dev@protonmail.com>
2023-08-17 13:17:35 -0600

    ♻️ Remove redundant code

--- app/main.py
+++ app/main.py
@@ -5,27 +5,25 @@ from flask import Flask, render_template
 
 load_dotenv()
 
+data = {
+    "profile": {
+        "name": os.getenv("PROFILE_NAME", ""),
+        "picture": os.getenv("PROFILE_PICTURE", ""),
+    }
+}
+
 app = Flask(__name__)
 
 
 @app.route("/")
 @app.route("/profile")
 def profile():
-    profile = {
-        "name": os.getenv("PROFILE_NAME", ""),
-    }
-    return render_template("profile.html", profile=profile)
+    return render_template("profile.html", profile=data["profile"])
 
 
 @app.route("/chat")
 def chat():
-    profile = {
-        "name": os.getenv("PROFILE_NAME", ""),
-        "picture": {
-            "url": "",
-        },
-    }
-    return render_template("chat.html", profile=profile)
+    return render_template("chat.html", profile=data["profile"])
 
 
 @app.route("/settings")
