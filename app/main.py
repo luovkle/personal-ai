ff9db7fc2570ddb238043293f4e8264d7d1e3bc3
luovkle <roberto.silva.dev@protonmail.com>
2023-08-19 22:18:40 -0600

    ✨ Add functionality to /profile endpoint

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import os
+import json
 from secrets import token_urlsafe
 from uuid import uuid4
 
@@ -7,6 +8,7 @@ from flask import Flask, render_template, session
 from flask_socketio import join_room, leave_room, send, SocketIO
 
 from app.utils.chatgpt import get_completion
+from app.utils.repository import get_repo_data
 
 load_dotenv()
 
@@ -14,7 +16,15 @@ data = {
     "profile": {
         "name": os.getenv("PROFILE_NAME", ""),
         "picture": os.getenv("PROFILE_PICTURE", ""),
-    }
+        "role": os.getenv("PROFILE_ROLE", ""),
+        "about": os.getenv("PROFILE_ABOUT", ""),
+    },
+    "contact": {
+        "github": os.getenv("PROFILE_CONTACT_GITHUB", ""),
+        "linkedin": os.getenv("PROFILE_CONTACT_LINKEDIN", ""),
+    },
+    "resume": os.getenv("RESUME", ""),
+    "projects": json.loads(os.getenv("PROJECTS", "[]")),
 }
 
 app = Flask(__name__)
@@ -25,7 +35,14 @@ socketio = SocketIO(app)
 @app.route("/")
 @app.route("/profile")
 def profile():
-    return render_template("profile.html", profile=data["profile"])
+    projects = [get_repo_data(repo) for repo in data["projects"]]
+    return render_template(
+        "profile.html",
+        profile=data["profile"],
+        contact=data["contact"],
+        resume=data["resume"],
+        projects=projects,
+    )
 
 
 @app.route("/chat")
